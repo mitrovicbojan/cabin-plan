0f847ecc55507f046f8212e2c9382e7c8f9a8454
unknown <mitrovicbojan87@gmail.com>
2022-04-22 21:57:51 +0300

    funct

--- app.js
+++ app.js
@@ -4,87 +4,141 @@ const cabins = [
         guestOne: "John Doe",
         guestTwo: "Milenko",
         bed: "double",
-        category: "A",
+        category: "a",
         remarks: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt, animi.",
+        photo: "./pics/rangers.png",
     },
     {
         id: 2,
         guestOne: "John Doe",
         guestTwo: "Milenko",
         bed: "double",
-        category: "A",
+        category: "a",
         remarks: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt, animi.",
+        photo: "./pics/rangers.png",
     },
     {
         id: 3,
         guestOne: "Spiderman",
         guestTwo: "Mitsos",
         bed: "flex",
-        category: "A",
+        category: "a",
         remarks: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt, animi.",
+        photo: "./pics/rangers.png",
     },
     {
         id: 4,
         guestOne: "John Doe",
         guestTwo: "Koula",
         bed: "double",
-        category: "B",
+        category: "b",
         remarks: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt, animi.",
+        photo: "./pics/rangers.png",
     },
     {
         id: 5,
         guestOne: "John Doe",
         guestTwo: "Milenko",
         bed: "twin",
-        category: "B",
+        category: "b",
         remarks: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt, animi.",
+        photo: "./pics/rangers.png",
     },
     {
         id: 6,
         guestOne: "Ignjat",
         guestTwo: "Zombi",
         bed: "double",
-        category: "C",
+        category: "c",
         remarks: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt, animi.",
+        photo: "./pics/rangers.png",
     },
     {
         id: 7,
         guestOne: "Jesus",
         guestTwo: "Lebowski",
         bed: "twin",
-        category: "C",
+        category: "c",
         remarks: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt, animi.",
+        photo: "./pics/rangers.png",
     },
     {
         id: 8,
         guestOne: "John Doe",
         guestTwo: "Milenko",
         bed: "triple",
-        category: "P",
+        category: "p",
         remarks: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt, animi.",
+        photo: "./pics/rangers.png",
     },
     {
         id: 9,
         guestOne: "John Doe",
         guestTwo: "Milenko",
         bed: "double",
-        category: "P",
+        category: "p",
         remarks: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt, animi.",
+        photo: "./pics/rangers.png",
     },
     {
         id: 10,
         guestOne: "John Doe",
         guestTwo: "Milenko",
         bed: "double",
-        category: "P",
+        category: "p",
         remarks: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt, animi.",
+        photo: "./pics/rangers.png",
     },
     {
         id: 11,
         guestOne: "John Doe",
         guestTwo: "Milenko",
         bed: "double",
-        category: "P",
+        category: "p",
         remarks: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt, animi.",
+        photo: "./pics/rangers.png",
     },
 ]
+
+const cabinContainer = document.querySelector('.container');
+const filterBtn = document.querySelectorAll('.cabin-btn');
+
+window.addEventListener('DOMContentLoaded', function() {
+    showCabinItems(cabins)
+})
+
+filterBtn.forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+        const category = e.currentTarget.dataset.id
+
+        const cabinCategory = cabins.filter(function(cabinItem) {
+            if(cabinItem.category === category) {
+                return cabinItem
+            }
+        })
+        if (category === 'all') {
+            showCabinItems(cabins)
+        }
+        else {
+            showCabinItems(cabinCategory)
+        }
+    })
+})
+
+function showCabinItems(cabinItems) {
+    let showCabin = cabinItems.map(function(item) {
+        return `<div class="cabin-item">
+        <img src=${item.photo} class="cabin-img" alt="pic" />
+        <div class="cabin-details">
+            <div class="cabin-pax">
+                <h1>${item.category}</h1>
+                <h3>${item.guestOne}</h3>
+                <h3>${item.guestTwo}</h3>
+            </div>
+            <p>Remarks: ${item.remarks}</p>
+        </div>
+    </div>`
+    });
+    showCabin = showCabin.join('');
+    cabinContainer.innerHTML = showCabin
+}
